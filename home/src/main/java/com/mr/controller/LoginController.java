5b0276c657e998102d1fb101af310fe127c0a788
ljs-lijunshuai <278290151@qq.com>
2019-10-26 08:18:50 +0800

    李军帅(更新)

--- home/src/main/java/com/mr/controller/LoginController.java
+++ home/src/main/java/com/mr/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.mr.controller;
 
 import com.mr.service.impl.HomeServiceImpl;
+import com.mr.util.CommonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 /**
@@ -15,5 +16,16 @@ public class LoginController {
 
     @Autowired
     private HomeServiceImpl service;
+
+    /**
+     * @Description : login登录
+     * @author: 李军帅
+     * @date :2019/10/25
+     * @param :[]
+     * @return :com.mr.util.CommonResult
+     */
+    public CommonResult login(){
+        return null;
+    }
 
 }
