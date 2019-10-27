2a3646082295ca0adb675eb62f82e37e2bfb6966
ljs-lijunshuai <278290151@qq.com>
2019-10-27 15:57:06 +0800

    aaa

--- home/src/main/java/com/mr/service/impl/HomeServiceImpl.java
+++ home/src/main/java/com/mr/service/impl/HomeServiceImpl.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class HomeServiceImpl implements IHomeService {
 
-    @Autowired
-    private UmsAdminLoginLogMapper umsAdminLoginLogMapper;
+    /*@Autowired
+    private UmsAdminLoginLogMapper umsAdminLoginLogMapper;*/
 }
